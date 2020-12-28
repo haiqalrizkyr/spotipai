6a933977a58af3fe96b0dbb5d3d94d9df1252794
Huzaifah M. Lais Lubis 191402120 <66837348+Huzaifahlais@users.noreply.github.com>
2020-12-28 21:19:05 +0700

    Update album.php

--- admin/album.php
+++ admin/album.php
@@ -3,20 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Album </title>
+    <title>Artist </title>
 </head>
 <body>
-    <h2 align="center">Album Musik</h2>
+    <h2 align="center">Artist</h2>
     <br>
 	<div class="container">
     <table class="table table-bordered">
-	<a href="index.php?halaman=tambahalbum" id="right" class="btn btn-success"><i class='fas fa-folder-plus'></i> Tambah Album</a>
+	<a href="index.php?halaman=tambahartist" id="right" class="btn btn-success"><i class='fas fa-folder-plus'></i> Tambah Artist</a>
 	<br><br>
 	
 	<thead>
 		<tr>
 			<th>No</th>
-			<th>album</th>
+			<th>artist</th>
 			<th>aksi</th>
 		</tr>
 	</thead>
@@ -24,10 +24,10 @@
 
 		<tr>
 			<td>1</td>
-			<td>monokrom</td>
+			<td>Adam Levine</td>
 			<td>
-			<a href="index.php?halaman=editalbum" class="btn btn-warning"><i class='fas fa-edit' ></i> edit</a>
-			<a href="index.php?halaman=hapusalbum" class="btn-danger btn"><i class='fas fa-trash-alt'></i> hapus</a>
+			<a href="index.php?halaman=editartist" class="btn btn-warning"><i class='fas fa-edit' ></i> edit</a>
+			<a href="index.php?halaman=hapusartist" class="btn-danger btn"><i class='fas fa-trash-alt'></i> hapus</a>
 			</td>
 		</tr>
 		
