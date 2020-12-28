c63481ff1e23851a14a85b72cd1d569f3eb8dee1
Huzaifah M. Lais Lubis 191402120 <66837348+Huzaifahlais@users.noreply.github.com>
2020-12-28 22:17:40 +0700

    Update daftar_lagu.php

--- admin/daftar_lagu.php
+++ admin/daftar_lagu.php
@@ -3,13 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.datatables.net/1.10.22/css/jquery.dataTables.min.css" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
     <title>Daftar Lagu</title>
 </head>
 <body>
     <h2 align="center">Daftar Lagu</h2>
     <br>
 	<div class="container">
-    <table class="table table-bordered">
+    <table id="example" class="table table-bordered">
 	<a href="index.php?halaman=tambahlagu" id="right" class="btn btn-success"><i class='fas fa-folder-plus'></i> Tambah Lagu</a>
 	<br><br>
 	
@@ -20,7 +24,7 @@
 			<th>Singer</th>
 			<th>Writer</th>
 			<th>Genre</th>
-			<th>aksi</th>
+			<th style="width: 200px;">aksi</th>
 		</tr>
 	</thead>
     <tbody>
@@ -40,6 +44,12 @@
 		
 
 	</tbody>
-</table>    
+</table>
+<script type="text/javascript">
+    $(document).ready(function() {
+         $('#example').DataTable()
+
+    })
+</script>
 </body>
 </html>
