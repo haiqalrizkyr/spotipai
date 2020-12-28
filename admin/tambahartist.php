1c7538a626f29dd6cb88b6a347f37a228369d0b1
Huzaifah M. Lais Lubis 191402120 <66837348+Huzaifahlais@users.noreply.github.com>
2020-12-28 21:24:31 +0700

    Update and rename tambahalbum.php to tambahartist.php

--- admin/tambahartist.php
+++ admin/tambahartist.php
@@ -2,17 +2,17 @@
 	#small{
   	color: red;}
 </style>
-<h2 align="center">Tambah Album</h2>
+<h2 align="center">Tambah Artist</h2>
 
 <div class="container">
 <div class="jumbotron">
 	<form method="post" enctype="multipart/form-data">
 	<div class="form-group">
-	 	<label class="col-sm-4 control-label">Album<small id="small"> *</small></label>
+	 	<label class="col-sm-4 control-label">Artist<small id="small"> *</small></label>
 	 	<input type="text" class="form-control"  name="genre"  required></input>
 	 </div>
 	 <small id="small">* Wajib Diisi</small>
 	 <br><br>
-	 <a  href="index.php?halaman=album" class="btn btn-info"><i class='fas fa-reply' ></i> BACK</span></a>
+	 <a  href="index.php?halaman=artist" class="btn btn-info"><i class='fas fa-reply' ></i> BACK</span></a>
 	 <button class ="btn btn-primary" name="save"><i class='fas fa-save'></i> SAVE</button>
 	</form>
