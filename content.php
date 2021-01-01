352a670b0db4b68d2b05ab641a6b46a143ce782b
Haiqal Rizky R. 191402054 <56826724+haiqalrizkyr@users.noreply.github.com>
2021-01-01 23:32:53 +0700

    penambahan beberapa fitur dan halaman

--- content.php
+++ content.php
@@ -1,33 +1,31 @@
 <div class="content">
 	<h1 style="color: white; margin-top: 70px;">For You</h1>
-	<div class="row row-cols-1 row-cols-md-3 g-4">
-		<div class="col">
-			<div class="card" style="background-color: black; color: white">
-				<img src="img/pic2.JPG" class="card-img-top" alt="..." height="400">
-				<div class="card-body">
-					<h5 class="card-title">Song Title</h5>
-					<p class="card-text">Artist</p>
-					<button class="btn btn-success">Play</button>
-				</div>
-			</div>
-		</div>
-		<div class="col">
-			<div class="card h-100" style="background-color: black; color: white">
-				<img src="img/pic2.JPG" class="card-img-top" alt="..." height="400">
-				<div class="card-body">
-					<h5 class="card-title">Song Title</h5>
-					<p class="card-text">Artist</p>
-				</div>
-			</div>
-		</div>
-		<div class="col">
-			<div class="card h-100" style="background-color: black; color: white">
-				<img src="img/pic2.JPG" class="card-img-top" alt="..." height="400">
-				<div class="card-body">
-					<h5 class="card-title">Song Title</h5>
-					<p class="card-text">Artist</p>
-				</div>
-			</div>
-		</div>
+	<div class="container">
+		<table class="table table-dark table-hover">
+			<thead>
+				<tr>
+					<th scope="col">#</th>
+					<th scope="col">Title</th>
+					<th scope="col">Artist</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<th scope="row">1</th>
+					<td><a href=""> Mark</td>
+					<td>Otto</td>
+				</tr>
+				<tr>
+					<th scope="row">2</th>
+					<td>Jacob</td>
+					<td>Thornton</td>
+				</tr>
+				<tr>
+					<th scope="row">3</th>
+					<td>Larry the Bird</td>
+					<td>Larry</td>
+				</tr>
+			</tbody>
+		</table>
 	</div>
 </div>
---
+++ search-content.php
@@ -0,0 +1,63 @@
+<style>
+    .container button {
+        border-radius: 35px;
+        border-color: transparent;
+        padding-left: 25px;
+        padding-top: 25px;
+        padding-right: 300px;
+        padding-bottom: 200px;
+        font-size: 35px;
+        background-size: 200px;
+        color: white;
+    }
+
+    #container2 button {
+        border-radius: 35px;
+        border-color: transparent;
+        padding-left: 25px;
+        padding-top: 25px;
+        padding-right: 100px;
+        padding-bottom: 100px;
+        font-size: 35px;
+        background-size: 200px;
+        color: white;
+    }
+
+    #container3 button {
+        border-radius: 35px;
+        border-color: transparent;
+        padding-left: 25px;
+        padding-top: 25px;
+        padding-right: 300px;
+        padding-bottom: 50px;
+        font-size: 35px;
+        background-size: 200px;
+        color: white;
+    }
+</style>
+<div class="content">
+    <div class="md-form mt-0">
+        <table style="margin-top: 70px;" align="center">
+            <tr>
+                <td><input class="form-control" type="text" placeholder="Search" aria-label="Search"></td>
+                <td><button class="btn btn-outline-success">Submit</button></td>
+            </tr>
+        </table>
+    </div>
+    <h1 style="color: white; margin-top: 10px;">Genres</h1>
+    <div class="container">
+        <button style="background-color: #286841">Pop</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <button style="background-color: #282E68">Rock</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <button style="background-color: #68284E">Jazz</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <h3><a href="genres.php" style="color:white;">Browse All Genres</a></h3>
+    </div>
+    <h1 style="color: white; margin-top: 10px;">Albums</h1>
+    <div class="container" id="container2">
+        <button style="background-color: #286841">Album</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <button style="background-color: #282E68">Album</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <button style="background-color: #65B748">Album</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <button style="background-color: #C9368D">Album</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <button style="background-color: #D629BA">Album</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <h3><a href="albums.php" style="color: white;">Browse All Albums</a></h3>
+    </div>
+</div>
